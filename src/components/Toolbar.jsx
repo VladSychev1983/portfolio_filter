6cbc664aed3d2f910deb4336fc7dbb8c65b9baa5
Vladislav Sychev <vladsychev1983@gmail.com>
2025-09-08 15:05:39 +0300

    created empty Toolbar component

---
+++ src/components/Toolbar.jsx
@@ -0,0 +1,6 @@
+export default function Toolbar({filters, selected, onSelectFilter}) {
+    return (
+        //filter buttons here.
+        <div className="className"><p>filter buttons</p></div>
+    );
+}
